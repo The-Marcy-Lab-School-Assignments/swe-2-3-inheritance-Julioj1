b11c3696b85672735ceaaead18455a06a90180fd
Julioj1 <dbjulioj@gmail.com>
2025-11-10 21:44:22 -0500

    Completed Quadrilateral

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,13 @@
 class Quadrilateral {
-
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+    this.side4 = side4;
+  }
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3 + this.side4;
+  }
 }
 
 class Rectangle {
